ebe41abfe75a282d838b9a87d6439e8277824bb7
jhoaho <jhoaho.sanchez@epn.edu.ec>
2023-12-06 11:23:35 -0500

    Actualizacion 2

--- 02-ANDROID/app/src/main/java/com/example/b2023gr2sw/MainActivity.kt
+++ 02-ANDROID/app/src/main/java/com/example/b2023gr2sw/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.b2023gr2sw
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,5 +18,16 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val botonCicloVida = findViewById<Button>(R.id.btn_ciclo_vida)
+        botonCicloVida
+            .setOnClickListener {
+                irActividad(ACicloVida::class.java)
+            }
+    }
+    fun irActividad(
+        clase: Class<*>
+    ){
+        val intent = Intent(this, clase)
+        startActivity(intent)
     }
 }
